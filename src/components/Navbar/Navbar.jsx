408a06133025c20c2fa7a007d2c611be0e325192
Rishika <rishikagoyal175@gmail.com>
2024-10-04 20:29:47 +0530

    done

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './Navbar.styles.css';
+
+const Navbar=()=>{
+    return(
+        <>
+        
+        <div className={styles.nav}>
+            <p className={styles.para}> The health and well-being of our patients and their health care team will always be our priority, so we follow the best practices for cleanliness.</p>
+        </div>
+        <div className={styles.heroSection}>
+        
+
+        </div>
+        </>
+    )
+}
+
+export default Navbar;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../pages/Home';
+
+const Home=()=>{
+    return(
+        <>
+        <Navbar/>
+        </>
+    )
+}
+
+export default Home;
